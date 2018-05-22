742e5026f150579e1efca71ef4648094dd7995db
yujun <yu.jun.reach@gmail.com>
2018-05-22 20:29:39 +0800

    添加git配置

---
+++ cpenv.sh
@@ -0,0 +1,5 @@
+ln -s git/linux-config/vim/.vundle_vimrc  ~/.vundle_vimrc
+ln -s git/linux-config/vim/.vimrc  ~/.vimrc
+ln -s git/linux-config/tmux/.tmux.conf  ~/.tmux.conf
+ln -s git/linux-config/git/.gitignore_global   ~/.gitignore_global
+ln -s git/linux-config/git/.gitconfig  ~/.gitconfig
