56bfe3812f4260a0b14fe5ca7a7e345ede96eaa2
reachfish <yu.jun.reach@gmail.com>
2022-12-07 15:56:04 +0800

    update vim

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s ~/git/linux-config/vim/.vimrc ~/.vimrc
-ln -s ~/git/linux-config/vim/.vundle_vimrc ~/.vundle_vimrc
+declare -a vimcfgs=("vundle_vimrc"  "ycm_vimrc"  "ycm_extra_conf.py")
+for file in "${vimcfgs[@]}"
+do
+    ln -s ~/git/linux-config/vim/${file} ~/.vim/${file}
+done
 
 pip3 install --user pathlib
 python3 ~/git/linux-config/vim-plugin-diff/apply_diff.py
 
 ln -s ~/git/linux-config/tmux/.tmux.conf ~/.tmux.conf
-
 
